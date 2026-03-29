01f0aff651b99f8e98d286b02f559da62aec34aa
D4nRusu <rusu.danmihai12@gmail.com>
2026-03-29 15:38:11 +0300

    fixed some fields that lead to server errors

--- api/src/main/java/com/urban_events/api/controller/AuthController.java
+++ api/src/main/java/com/urban_events/api/controller/AuthController.java
@@ -1,12 +1,14 @@
 package com.urban_events.api.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.urban_events.api.service.UserService;
 import com.urban_events.api.dto.RegisterRequest;
+import com.urban_events.api.model.User;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,8 +19,8 @@ public class AuthController {
 
     private final UserService userService;
 
-    @GetMapping("/register")
-    public ResponseEntity<String> register(RegisterRequest request) {
-        return ResponseEntity.ok(userService.registerUser(request).getEmail() + " registered successfully");
+    @PostMapping("/register")
+    public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(userService.registerUser(request));
     }
 }
